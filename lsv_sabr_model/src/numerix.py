44b1eec112f17db38123a5d26ed08fe004c56920
Giuseppe Trapani <trpgsp@gmail.com>
2019-06-16 20:46:48 +0200

    Numerix functions are all working and up to date

--- lsv_sabr_model/src/numerix.py
+++ lsv_sabr_model/src/numerix.py
@@ -3,19 +3,19 @@ import numba as nb
 from numba import cfunc, carray, types, vectorize, guvectorize, njit, float64
 import numpy as np
 from scipy import LowLevelCallable
-from scipy.integrate import quad
+from scipy.integrate import quad, quadrature
 
 @njit
 def lv_sigma(u, beta_low, beta_high, eps, eps_s, f_low, f_high, lambda_, Psi, K_h, F0):
     """
     Argument of the Lamperti's transform that returns the local displacement
-    of the stochastic volatility function.
+    of the stochastic volatility function. Inverse of Def (21)
     """
     sigma_argument = min(max(u, f_low), f_high)
     heta_1 = eps * math.log(1 + math.exp(sigma_argument/eps))
     heta_2 = eps_s * math.log(1 + math.exp((sigma_argument - K_h)/eps_s))
     beta_factor = math.tanh(lambda_*(sigma_argument - F0))
-    beta = 0.5*(beta_high + beta_low) - 0.5*(beta_high - beta_low)*beta_factor
+    beta = 0.5*(beta_high + beta_low) + 0.5*(beta_high - beta_low)*beta_factor
     return 1.0/(math.pow(heta_1, beta) + Psi*heta_2)
 
 @njit
@@ -29,55 +29,70 @@ def lv_sigma_vectorized(u_array, beta_low, beta_high, eps, eps_s, f_low, f_high,
         sigma_output[i] = lv_sigma(u, beta_low, beta_high, eps, eps_s, f_low, f_high, lambda_, Psi, K_h, F0)
     return sigma_output
 
-
-lv_sigma_csignature = types.double(types.intc, types.CPointer(types.double))
-@cfunc(lv_sigma_csignature, nopython=True)
-def lv_sigma_cfunc(n, xx):
-    in_array = carray(xx, (n, ))
-    u, beta_low, beta_high, eps, eps_s, f_low, f_high, lambda_, Psi, K_h, F0 = in_array
-    sigma_argument = min(max(u, f_low), f_high)
-    heta_1 = eps * math.log(1 + math.exp(sigma_argument/eps))
-    heta_2 = eps_s * math.log(1 + math.exp((sigma_argument - K_h)/eps_s))
-    beta_factor = math.tanh(lambda_*(sigma_argument - F0))
-    beta = 0.5*(beta_high + beta_low) - 0.5*(beta_high - beta_low)*beta_factor
-    return 1.0/(math.pow(heta_1, beta) + Psi*heta_2)
-
-lv_sigma_integrand_scipy = LowLevelCallable(lv_sigma_cfunc.ctypes)
-
 @njit
 def lamperti_transform_lv_sigma(upper_bound, lower_bound, beta_low, beta_high, eps, eps_s, f_low, f_high, lambda_, Psi, K_h, F0):
     """
     Local volatility function obtained as Lamperti's transform of the
-    underlying displacement function.
+    underlying displacement function. Def Int_F0^f(1/(sigma(u)))du 
+    pag 2 in Bang(2018). This function performs the integral numerically
+    with a Gaussian Quadrature over 64 points. Points and weights in the
+    (1, 1) interval are cached. The integral is then computed by changing 
+    the integration interval from (-1, 1) to (lower_bound, upper_bound).
     """
     u_points = np.array(
-        [-0.99930504, -0.99634012, -0.99101337, -0.98333625, -0.97332683,
-       -0.9610088 , -0.94641137, -0.92956917, -0.91052214, -0.88931545,
-       -0.8659994 , -0.8406293 , -0.81326532, -0.78397236, -0.75281991,
-       -0.71988185, -0.68523631, -0.64896547, -0.61115536, -0.57189565,
-       -0.53127946, -0.48940315, -0.44636602, -0.40227016, -0.35722016,
-       -0.31132287, -0.26468716, -0.21742364, -0.16964442, -0.12146282,
-       -0.07299312, -0.02435029,  0.02435029,  0.07299312,  0.12146282,
-        0.16964442,  0.21742364,  0.26468716,  0.31132287,  0.35722016,
-        0.40227016,  0.44636602,  0.48940315,  0.53127946,  0.57189565,
-        0.61115536,  0.64896547,  0.68523631,  0.71988185,  0.75281991,
-        0.78397236,  0.81326532,  0.8406293 ,  0.8659994 ,  0.88931545,
-        0.91052214,  0.92956917,  0.94641137,  0.9610088 ,  0.97332683,
-        0.98333625,  0.99101337,  0.99634012,  0.99930504])
+                [-0.99982489, -0.99907746, -0.99773325, -0.99579276, -0.99325711,
+                -0.99012782, -0.98640674, -0.98209611, -0.97719849, -0.97171682,
+                -0.96565437, -0.95901476, -0.95180196, -0.94402029, -0.93567439,
+                -0.92676925, -0.9173102 , -0.90730288, -0.89675329, -0.88566772,
+                -0.8740528 , -0.86191547, -0.84926299, -0.83610292, -0.82244312,
+                -0.80829176, -0.79365729, -0.77854848, -0.76297433, -0.74694417,
+                -0.73046757, -0.71355438, -0.69621471, -0.67845892, -0.66029763,
+                -0.64174169, -0.62280219, -0.60349046, -0.58381802, -0.56379665,
+                -0.5434383 , -0.52275515, -0.50175956, -0.48046407, -0.45888142,
+                -0.4370245 , -0.41490638, -0.39254028, -0.36993956, -0.34711773,
+                -0.32408844, -0.30086544, -0.27746262, -0.25389397, -0.23017356,
+                -0.20631559, -0.18233431, -0.15824404, -0.1340592 , -0.10979423,
+                -0.08546364, -0.06108197, -0.03666379, -0.0122237 ,  0.0122237,
+                0.03666379,  0.06108197,  0.08546364,  0.10979423,  0.1340592,
+                0.15824404,  0.18233431,  0.20631559,  0.23017356,  0.25389397,
+                0.27746262,  0.30086544,  0.32408844,  0.34711773,  0.36993956,
+                0.39254028,  0.41490638,  0.4370245 ,  0.45888142,  0.48046407,
+                0.50175956,  0.52275515,  0.5434383 ,  0.56379665,  0.58381802,
+                0.60349046,  0.62280219,  0.64174169,  0.66029763,  0.67845892,
+                0.69621471,  0.71355438,  0.73046757,  0.74694417,  0.76297433,
+                0.77854848,  0.79365729,  0.80829176,  0.82244312,  0.83610292,
+                0.84926299,  0.86191547,  0.8740528 ,  0.88566772,  0.89675329,
+                0.90730288,  0.9173102 ,  0.92676925,  0.93567439,  0.94402029,
+                0.95180196,  0.95901476,  0.96565437,  0.97171682,  0.97719849,
+                0.98209611,  0.98640674,  0.99012782,  0.99325711,  0.99579276,
+                0.99773325,  0.99907746,  0.99982489])
     w_i = np.array(
-        [0.00178328, 0.00414703, 0.00650446, 0.00884676, 0.01116814,
-       0.01346305, 0.01572603, 0.01795172, 0.02013482, 0.02227017,
-       0.0243527 , 0.02637747, 0.02833967, 0.03023466, 0.03205793,
-       0.03380516, 0.03547221, 0.03705513, 0.03855015, 0.03995374,
-       0.04126256, 0.04247352, 0.04358372, 0.04459056, 0.04549163,
-       0.0462848 , 0.04696818, 0.04754017, 0.04799939, 0.04834476,
-       0.04857547, 0.04869096, 0.04869096, 0.04857547, 0.04834476,
-       0.04799939, 0.04754017, 0.04696818, 0.0462848 , 0.04549163,
-       0.04459056, 0.04358372, 0.04247352, 0.04126256, 0.03995374,
-       0.03855015, 0.03705513, 0.03547221, 0.03380516, 0.03205793,
-       0.03023466, 0.02833967, 0.02637747, 0.0243527 , 0.02227017,
-       0.02013482, 0.01795172, 0.01572603, 0.01346305, 0.01116814,
-       0.00884676, 0.00650446, 0.00414703, 0.00178328])
+                [0.00044938, 0.00104581, 0.0016425 , 0.00223829, 0.00283275,
+                0.00342553, 0.00401625, 0.00460458, 0.00519016, 0.00577264,
+                0.00635166, 0.00692689, 0.00749798, 0.00806459, 0.00862638,
+                0.00918301, 0.00973415, 0.01027948, 0.01081866, 0.01135138,
+                0.01187731, 0.01239614, 0.01290756, 0.01341127, 0.01390696,
+                0.01439435, 0.01487312, 0.01534301, 0.01580373, 0.016255,
+                0.01669656, 0.01712814, 0.01754948, 0.01796033, 0.01836044,
+                0.01874959, 0.01912752, 0.01949403, 0.01984888, 0.02019187,
+                0.02052279, 0.02084145, 0.02114765, 0.02144121, 0.02172195,
+                0.02198971, 0.02224433, 0.02248565, 0.02271354, 0.02292784,
+                0.02312845, 0.02331523, 0.02348808, 0.02364688, 0.02379156,
+                0.02392201, 0.02403817, 0.02413996, 0.02422732, 0.0243002 ,
+                0.02435856, 0.02440236, 0.02443157, 0.02444618, 0.02444618,
+                0.02443157, 0.02440236, 0.02435856, 0.0243002 , 0.02422732,
+                0.02413996, 0.02403817, 0.02392201, 0.02379156, 0.02364688,
+                0.02348808, 0.02331523, 0.02312845, 0.02292784, 0.02271354,
+                0.02248565, 0.02224433, 0.02198971, 0.02172195, 0.02144121,
+                0.02114765, 0.02084145, 0.02052279, 0.02019187, 0.01984888,
+                0.01949403, 0.01912752, 0.01874959, 0.01836044, 0.01796033,
+                0.01754948, 0.01712814, 0.01669656, 0.016255, 0.01580373,
+                0.01534301, 0.01487312, 0.01439435, 0.01390696, 0.01341127,
+                0.01290756, 0.01239614, 0.01187731, 0.01135138, 0.01081866,
+                0.01027948, 0.00973415, 0.00918301, 0.00862638, 0.00806459,
+                0.00749798, 0.00692689, 0.00635166, 0.00577264, 0.00519016,
+                0.00460458, 0.00401625, 0.00342553, 0.00283275, 0.00223829,
+                0.0016425 , 0.00104581, 0.00044938])
     
     u_i = 0.5*((upper_bound - lower_bound)*u_points + (lower_bound + upper_bound))
     f_xi = lv_sigma_vectorized(u_i, beta_low, beta_high, eps, eps_s, f_low, f_high, lambda_, Psi, K_h, F0)
@@ -116,7 +131,7 @@ def gamma_big(lambda_, k, h_cut, time_to_maturity):
     Auxiliary function for the computation of the CDF of the underlying 
     under displaced-stochastic volatility model. Refer to (19) in Bang (2018).
     """
-    if k >= -   h_cut:
+    if k >= - h_cut:
         exponential = 0.125*lambda_*lambda_*time_to_maturity - 0.5*lambda_*h_cut
         sqrt_T = math.sqrt(time_to_maturity)
         d1 = -k/sqrt_T - 0.5*lambda_*sqrt_T
@@ -134,30 +149,32 @@ def one_minus_CDF(strike, forward_rate, time_to_maturity,
     CDF of the process underlying the Sabr normal approximation
     by Bang (2018).
     """
-    z_k = (strike - forward_rate)*(nu/alpha)
-    xhi_k = math.sqrt(1 - rho*rho + (z_k + rho)*(z_k + rho)) + z_k + rho 
+    z_k = (strike - forward_rate)*(nu/alpha) # def Lemma2
+    z_k_plus_rho = z_k + rho
+    xhi_k = math.sqrt(1 - rho*rho + z_k_plus_rho*z_k_plus_rho) + z_k_plus_rho
     xhi_k /= 1 + rho
 
-    h_cut = math.log((1 + rho)/(1 - rho))/(2 * nu)
+    h_cut = math.log((1 + rho)/(1 - rho))/(2 * nu) # def (10)
 
-    gamma = 1.5*gamma_star(nu, h_cut, time_to_maturity) - 0.5*gamma_star(3*nu, h_cut, time_to_maturity)
+    gamma = 1.5*gamma_star(nu, h_cut, time_to_maturity) # def (15)
+    gamma -= 0.5*gamma_star(3*nu, h_cut, time_to_maturity) 
 
-    E_factor = 0.5*math.exp(nu*nu*time_to_maturity*0.5)/gamma
+    E_factor = 0.5*math.exp(nu*nu*time_to_maturity*0.5)/gamma # def (17)
 
     E_plus = 3*gamma_star(nu, h_cut + nu*time_to_maturity, time_to_maturity)
     E_plus -= gamma_star(3*nu, h_cut + nu*time_to_maturity, time_to_maturity)
-    E_plus *= E_factor
+    E_plus *= E_factor # def (17), positive
 
     E_minus = 3*gamma_star(nu, h_cut - nu*time_to_maturity, time_to_maturity)
     E_minus -= gamma_star(3*nu, h_cut - nu*time_to_maturity, time_to_maturity)
-    E_minus *= E_factor
+    E_minus *= E_factor # def (17), negative
 
-    Gamma = rho + math.sqrt(rho*rho + (1 - rho)*(1 - rho)*E_plus*E_minus)
-    Gamma *= (1 + rho)*E_plus
+    Gamma = rho + math.sqrt(rho*rho + (1 - rho*rho)*E_plus*E_minus) # def (16)
+    Gamma /= (1 + rho)*E_plus
 
-    k = math.log(xhi_k/Gamma)/nu
+    k = math.log(xhi_k/Gamma)/nu # def (14)
 
-    out = 1.25*gamma_big(nu, k, h_cut, time_to_maturity)
+    out = 1.5*gamma_big(nu, k, h_cut, time_to_maturity)
     out -= 0.5*gamma_big(3*nu, k, h_cut, time_to_maturity)
     return out/gamma
 
@@ -166,50 +183,65 @@ def one_minus_CDF_underlying(strike, forward_rate, time_to_maturity,
                                 alpha, nu, rho, mu,
                                 beta_low, beta_high, eps, eps_s, f_low, f_high, lambda_, Psi, K_h):
     """
-    CDF of the locally displaced underlyng process in the LV-Sabr model
-    by Bang et al.
+    One minus CDF of the locally displaced underlyng process in the LV-Sabr model
+    by Bang et al. Argument of Def (5) for Call prices.
     """
-    displaced_strike = lamperti_transform_lv_sigma(strike, forward_rate, 
-                                                    beta_low, beta_high, eps, eps_s, 
-                                                    f_low, f_high, lambda_, Psi, K_h, forward_rate)
-    displaced_strike += mu
+    G_k = lamperti_transform_lv_sigma(strike, forward_rate, 
+                                        beta_low, beta_high, eps, eps_s, 
+                                        f_low, f_high, lambda_, Psi, K_h, forward_rate)
+    displaced_strike = G_k + mu
     return one_minus_CDF(displaced_strike, forward_rate, time_to_maturity, alpha, nu, rho)
 
 @njit
 def CDF_underlying(strike, forward_rate, time_to_maturity,
                     alpha, nu, rho, mu,
                     beta_low, beta_high, eps, eps_s, f_low, f_high, lambda_, Psi, K_h):
-    displaced_strike = lamperti_transform_lv_sigma(strike, forward_rate, 
-                                                    beta_low, beta_high, eps, eps_s, 
-                                                    f_low, f_high, lambda_, Psi, K_h, forward_rate)
-    displaced_strike += mu
+    """
+    CDF of the locally displaced underlyng process in the LV-Sabr model
+    by Bang et al. Argument of Def (5) for Put prices.
+    """
+    G_k = lamperti_transform_lv_sigma(strike, forward_rate, 
+                                        beta_low, beta_high, eps, eps_s,
+                                        f_low, f_high, lambda_, Psi, K_h, forward_rate)
+    displaced_strike = G_k + mu
     return 1.0 - one_minus_CDF(displaced_strike, forward_rate, time_to_maturity, alpha, nu, rho)
-
 
 def call_terminal_value(strike, forward_rate, time_to_maturity,
                         alpha, nu, rho, mu,
                         beta_low, beta_high, eps, eps_s, f_low, f_high, lambda_, Psi, K_h):
     """
     Undiscounted value of the Call price under the LV-Sabr model 
-    by Bang (2018).
+    by Bang (2018). Def (5)
     """
-    extra_args = (forward_rate, time_to_maturity, 
+    extra_args = (forward_rate, time_to_maturity,
                     alpha, nu, rho, mu, 
                     beta_low, beta_high, eps, eps_s, f_low, f_high, lambda_, Psi, K_h)
-    return quad(one_minus_CDF_underlying, a=strike, b=np.inf, args=extra_args)
+    return quad(one_minus_CDF_underlying, a=strike, b=np.inf, epsabs=1e-6, epsrel=1e-6, args=extra_args, limit=100)
 
 def put_terminal_value(strike, forward_rate, time_to_maturity,
                         alpha, nu, rho, mu,
                         beta_low, beta_high, eps, eps_s, f_low, f_high, lambda_, Psi, K_h):
     """
     Undiscounted value of the Put price under the LV-Sabr model
-    by Bang (2018).
+    by Bang (2018). Def (5)
     """
     extra_args = (forward_rate, time_to_maturity, 
                     alpha, nu, rho, mu, 
                     beta_low, beta_high, eps, eps_s, f_low, f_high, lambda_, Psi, K_h)
-    return quad(CDF_underlying, a=-np.inf, b=strike, args=extra_args)
+    return quad(CDF_underlying, a=-np.inf, b=strike, epsabs=1e-6, epsrel=1e-6, args=extra_args, limit=100)
 
+#### Extra function for checking integration is correct
+lv_sigma_csignature = types.double(types.intc, types.CPointer(types.double))
+@cfunc(lv_sigma_csignature, nopython=True)
+def lv_sigma_cfunc(n, xx):
+    in_array = carray(xx, (n, ))
+    u, beta_low, beta_high, eps, eps_s, f_low, f_high, lambda_, Psi, K_h, F0 = in_array
+    sigma_argument = min(max(u, f_low), f_high)
+    heta_1 = eps * math.log(1 + math.exp(sigma_argument/eps))
+    heta_2 = eps_s * math.log(1 + math.exp((sigma_argument - K_h)/eps_s))
+    beta_factor = math.tanh(lambda_*(sigma_argument - F0))
+    beta = 0.5*(beta_high + beta_low) - 0.5*(beta_high - beta_low)*beta_factor
+    return 1.0/(math.pow(heta_1, beta) + Psi*heta_2)
 
-
+lv_sigma_integrand_scipy = LowLevelCallable(lv_sigma_cfunc.ctypes)
 
--- lsv_sabr_model/test.py
+++ lsv_sabr_model/test.py
@@ -3,6 +3,7 @@ import math
 from datetime import datetime
 from dataclasses import dataclass
 import numpy as np
+from lmfit import Parameters, minimize
 from scipy.integrate import quad, fixed_quad
 from src.numerix import lv_sigma, lv_sigma_vectorized, lamperti_transform_lv_sigma, lv_sigma_integrand_scipy
 from src.numerix import one_minus_CDF_underlying, CDF_underlying
@@ -49,7 +50,6 @@ def test_functions_return_value(load_basic_data):
         raise RuntimeError
 
 def test_density_return_value(load_basic_data):
-    option_strike = 0.04
     f0 = load_basic_data.forward_rate
     extra_args = load_basic_data.get_params()
     a, v, p = 0.00533, 0.165, 0.07
@@ -126,24 +126,72 @@ def time_compute_call_and_put_terminal_values():
     fh = F0 + 0.5
     kh = F0 + 0.08
     option_strike = 0.03
-    test_data = TestData(bl, bh, e, es, psi, kh, lmbd, fl, fh, F0)
 
-    extra_args = test_data.get_params()
-
-    a, v, p, m = 0.00533, 0.165, 0.07, 0.01
-    ttm = 1.0
+    a, v, p, m = 0.009099, 0.165, 0.07, 0.02098
+    ttm = 10.0
 
     now = datetime.now()
-    print(call_terminal_value(option_strike, F0, ttm, a, v, p, m, bl, bh, e, es, fl, fh, lmbd, psi, kh))
     print(put_terminal_value(option_strike, F0, ttm, a, v, p, m, bl, bh, e, es, fl, fh, lmbd, psi, kh))
     print(datetime.now() - now)
- 
-    
+    now = datetime.now()
+    print(call_terminal_value(option_strike, F0, ttm, a, v, p, m, bl, bh, e, es, fl, fh, lmbd, psi, kh))
+    print(datetime.now() - now)
+
+def alpha_mu_calibration_obj_function(params, forward_rate, atm_price, time_to_maturity):
+    params_values_dict = params.valuesdict()
+    a = params_values_dict['alpha']
+    v = params_values_dict['nu']
+    p = params_values_dict['rho']
+    m = params_values_dict['mu']
+    bl = params_values_dict['beta_low']
+    bh = params_values_dict['beta_high']
+    e = params_values_dict['epsilon']
+    es = params_values_dict['epsilon_smoothing']
+    fl = params_values_dict['forward_low']
+    fh = params_values_dict['forward_high']
+    lmbd = params_values_dict['lambda_']
+    psi = params_values_dict['Psi']
+    kh = params_values_dict['K_h']
+    call_value = call_terminal_value(forward_rate, forward_rate, time_to_maturity, a, v, p, m, bl, bh, e, es, fl, fh, lmbd, psi, kh)[0]
+    put_value = put_terminal_value(forward_rate, forward_rate, time_to_maturity, a, v, p, m, bl, bh, e, es, fl, fh, lmbd, psi, kh)[0]
+    put_call_parity_error = call_value - put_value
+    atm_repricing_error = call_value - atm_price
+    return [put_call_parity_error, atm_repricing_error]
+
+def time_alpha_mu_calibration():
+    parameters_vect = Parameters()
+
+    parameters_vect.add('alpha', value=0.03, vary=True, min=1e-8)
+    parameters_vect.add('nu', value=0.165, vary=False, min=1e-8)
+    parameters_vect.add('rho', value=0.07, vary=False, min=-1.0, max=1.0)
+    parameters_vect.add('mu', value=0.12, vary=True)
+    parameters_vect.add('beta_low', value=0.265, vary=False)
+    parameters_vect.add('beta_high', value=0.0, vary=False)
+    parameters_vect.add('epsilon', value=0.0032, vary=False, min=0.0)
+    parameters_vect.add('epsilon_smoothing', value=0.001, vary=False, min=1e-8)
+    parameters_vect.add('forward_low', value=-0.03, vary=False)
+    parameters_vect.add('forward_high', value=0.51833, vary=False)
+    parameters_vect.add('lambda_', value=0.5, vary=False)
+    parameters_vect.add('Psi', value=47.7, vary=False)
+    parameters_vect.add('K_h', value=0.09833, vary=False)
+
+    F0 = 0.01833
+    ttm = 10.0
+    sigma_atm = 0.00533
+    atm_price = sigma_atm * math.sqrt(ttm / (2.0 * math.pi)) 
+
+    now = datetime.now()
+    opt_results = minimize(alpha_mu_calibration_obj_function, parameters_vect, args=(F0, atm_price, ttm))
+    opt_results.params.pretty_print()
+    print(opt_results.residual)
+    print(alpha_mu_calibration_obj_function(opt_results.params, F0, atm_price, ttm))
+    print(datetime.now() - now)
 
 
 if __name__ == '__main__':
     # time_compute_local_volatility_lamperti_transform()
-    time_compute_call_and_put_density()
-    print()
+    # print()
+    # time_compute_call_and_put_density()
+    # print()
     time_compute_call_and_put_terminal_values()
-
+    # time_alpha_mu_calibration()
